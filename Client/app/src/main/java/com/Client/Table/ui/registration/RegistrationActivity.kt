981258eb41a350b2b5d5c6e9639cc53a55bc1c5a
Dzem <rozajacdz@gmail.com>
2021-04-20 22:18:15 +0200

    L4G-1 added validation for input [TB, RDZ]

--- Client/app/src/main/java/com/Client/Table/ui/registration/RegistrationActivity.kt
+++ Client/app/src/main/java/com/Client/Table/ui/registration/RegistrationActivity.kt
@@ -2,29 +2,92 @@ package com.Client.Table.ui.registration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.Client.Table.R
 
+
 class RegistrationActivity : AppCompatActivity() {
+    lateinit var username: EditText
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var repeatPassword: EditText
+    val MIN_PASSWORD_LENGTH = 6;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-        var email = findViewById<EditText>(R.id.register_email)
-        var username = findViewById<EditText>(R.id.register_username)
-        var password = findViewById<EditText>(R.id.password)
-        var pass_repeat = findViewById<EditText>(R.id.password_repeat)
-        var btn_register = findViewById<Button>(R.id.register_btn)
+        viewInitializations();
+        validateInput();
+        performRegistration();
+    }
 
-        btn_register.setOnClickListener {
-            val email_check = email.text;
-            val username_check = username.text;
-            val password_check = password.text;
-            val pass_repeat_check = pass_repeat.text;
+    private fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 
-            
+    fun viewInitializations() {
+        username = findViewById(R.id.register_username)
+        email = findViewById(R.id.register_email)
+        password = findViewById(R.id.register_password)
+        repeatPassword = findViewById(R.id.password_repeat)
+    }
+
+    private fun validateInput(): Boolean {
+        if (username.getText().toString().equals("")) {
+            username.setError("Please Enter Last Name")
+            return false
+        }
+        if (email.getText().toString().equals("")) {
+            email.setError("Please Enter Email")
+            return false
+        }
+        if (password.getText().toString().equals("")) {
+            password.setError("Please Enter Password")
+            return false
+        }
+        if (repeatPassword.getText().toString().equals("")) {
+            repeatPassword.setError("Please Enter Repeat Password")
+            return false
         }
 
+        // checking the proper email format
+        if (!isEmailValid(email.getText().toString())) {
+            email.setError("Please Enter Valid Email")
+            return false
+        }
+
+        // checking minimum password Length
+        if (password.getText().length < MIN_PASSWORD_LENGTH) {
+            password.setError("Password Length must be more than " + MIN_PASSWORD_LENGTH + " characters")
+            return false
+        }
+
+        // Checking if repeat password is same
+        if (!password.getText().toString().equals(repeatPassword.text.toString())) {
+            repeatPassword.setError("Password does not match")
+            return false
+        }
+        return true
+    }
+
+    private fun performRegistration() {
+        if (validateInput()) {
+
+            // Input is valid, here send data to your server
+
+            val username = username.getText().toString()
+            val email = email.getText().toString()
+            val password = password.getText().toString()
+            val repeatPassword = repeatPassword.getText().toString()
+
+            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+            // Here you can call you API
+
+        }
     }
 }
