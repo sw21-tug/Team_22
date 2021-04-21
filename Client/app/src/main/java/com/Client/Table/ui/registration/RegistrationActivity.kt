70841e93241704b89563c135bc6526ceeeca8a01
Tarik Barucija <tarikbarucija@outlook.com>
2021-04-21 10:03:49 +0200

    L4G-1 added button and listener [RDZ, BT]

--- Client/app/src/main/java/com/Client/Table/ui/registration/RegistrationActivity.kt
+++ Client/app/src/main/java/com/Client/Table/ui/registration/RegistrationActivity.kt
@@ -11,19 +11,25 @@ import com.Client.Table.R
 
 
 class RegistrationActivity : AppCompatActivity() {
+
     lateinit var username: EditText
     lateinit var email: EditText
     lateinit var password: EditText
     lateinit var repeatPassword: EditText
+    lateinit var submitBtn: Button
     val MIN_PASSWORD_LENGTH = 6;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
-        viewInitializations();
-        validateInput();
-        performRegistration();
+        viewInitializations()
+        submitBtn = findViewById(R.id.register_btn)
+        submitBtn.setOnClickListener {
+            Toast.makeText(this, "You clicked me.", Toast.LENGTH_SHORT).show()
+            submitBtn.visibility = View.VISIBLE
+            performRegistration()
+        }
     }
 
     private fun isEmailValid(email: String): Boolean {
@@ -35,42 +41,43 @@ class RegistrationActivity : AppCompatActivity() {
         email = findViewById(R.id.register_email)
         password = findViewById(R.id.register_password)
         repeatPassword = findViewById(R.id.password_repeat)
+
     }
 
     private fun validateInput(): Boolean {
         if (username.getText().toString().equals("")) {
             username.setError("Please Enter Last Name")
-            return false
+            //return false
         }
         if (email.getText().toString().equals("")) {
             email.setError("Please Enter Email")
-            return false
+            //return false
         }
         if (password.getText().toString().equals("")) {
             password.setError("Please Enter Password")
-            return false
+            //return false
         }
         if (repeatPassword.getText().toString().equals("")) {
             repeatPassword.setError("Please Enter Repeat Password")
-            return false
+            //return false
         }
 
         // checking the proper email format
         if (!isEmailValid(email.getText().toString())) {
             email.setError("Please Enter Valid Email")
-            return false
+            //return false
         }
 
         // checking minimum password Length
         if (password.getText().length < MIN_PASSWORD_LENGTH) {
             password.setError("Password Length must be more than " + MIN_PASSWORD_LENGTH + " characters")
-            return false
+            //return false
         }
 
         // Checking if repeat password is same
         if (!password.getText().toString().equals(repeatPassword.text.toString())) {
             repeatPassword.setError("Password does not match")
-            return false
+            //return false
         }
         return true
     }
@@ -85,9 +92,11 @@ class RegistrationActivity : AppCompatActivity() {
             val password = password.getText().toString()
             val repeatPassword = repeatPassword.getText().toString()
 
-            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Registration Success", Toast.LENGTH_SHORT).show()
             // Here you can call you API
 
         }
     }
 }
+
+
