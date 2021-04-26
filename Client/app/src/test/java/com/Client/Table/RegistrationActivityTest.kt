af4668bd73d2286c711c1651c335a7e45b67e0ac
Jörg Werner Rainer <joerg.rainer@student.tugraz.at>
2021-04-26 11:02:44 +0200

    L4G-[1,3] Removed espresso tests from Junit directory [RJ]

--- Client/app/src/test/java/com/Client/Table/RegistrationActivityTest.kt
+++ Client/app/src/test/java/com/Client/Table/RegistrationActivityTest.kt
@@ -1,48 +1,14 @@
 package com.Client.Table
 
 import com.Client.Table.ui.registration.RegistrationActivity
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.closeSoftKeyboard
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAction
-import androidx.test.espresso.ViewInteraction
-import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.hasErrorText
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.Client.Table.R
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import androidx.test.espresso.action.ViewActions.typeText
 
 
 class RegistrationActivityTest {
-
     @Test
-    fun testInvalidUsername() {
-        val activityScenario = ActivityScenario.launch(RegistrationActivity::class.java)
-        onView(withId(R.id.register_username)).perform(typeText("ba"))
-        closeSoftKeyboard()
-        onView(withId(R.id.register_btn)).perform(click())
-        onView(withId(R.id.register_username)).check(matches(hasErrorText("Username Length must be more than 4 characters")))
-    }
-
-    @Test
-    fun testInvalidPassword() {
-        val activityScenario = ActivityScenario.launch(RegistrationActivity::class.java)
-        onView(withId(R.id.register_password)).perform(typeText("39"))
-        closeSoftKeyboard()
-        onView(withId(R.id.register_btn)).perform(click())
-        onView(withId(R.id.register_password)).check(matches(hasErrorText("Password Length must be more than 6 characters")))
-    }
-
-}
-
-class RegistrationActivityTestTwo {
-    @Test
-    fun testEmptyForm() {
-        val activityScenario = ActivityScenario.launch(RegistrationActivity::class.java)
-        onView(withId(R.id.register_btn)).perform(click())
-        onView(withId(R.id.register_username)).check(matches(hasErrorText("Username Length must be more than 4 characters")))
-        onView(withId(R.id.register_password)).check(matches(hasErrorText("Password Length must be more than 6 characters")))
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 }
