9fff4aa55782ef5abaf3ac3f2a385e3a3feba374
Christof Feischl <mail@christof-feischl.at>
2021-05-03 16:31:29 +0200

    L4G-8 Update all testcases to close the softkeyboard after typing in text [RDZ, FC]

--- Client/app/src/androidTest/java/com/Client/Table/registration/RegistrationUnitTest.kt
+++ Client/app/src/androidTest/java/com/Client/Table/registration/RegistrationUnitTest.kt
@@ -3,9 +3,8 @@ package com.Client.Table.registration
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.PerformException
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.Client.Table.ui.registration.RegistrationActivity
@@ -17,8 +16,8 @@ class RegistrationUnitTest {
     @Test
     fun testForRegistration() {
         val activityScenario = ActivityScenario.launch(RegistrationActivity::class.java)
-        onView(withId(R.id.register_email)).perform(typeText("tarik"))
-        onView(withId(R.id.register_btn)).perform(click())
+        onView(withId(R.id.register_email)).perform(typeText("tarik"), closeSoftKeyboard())
+        onView(withId(R.id.register_btn)).perform(click(), closeSoftKeyboard())
         onView(withId(R.id.register_email)).check(matches(hasErrorText("Please Enter Valid Email")))
 
     }
