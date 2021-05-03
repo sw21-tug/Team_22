88f1c1fb2d799df090500d0b8cd7b3b1ae6e9c0a
Christof Feischl <mail@christof-feischl.at>
2021-05-03 09:11:49 +0200

    L4G-8 Close keyboard in previous test after performing input operations [RDZ, FC]

--- Client/app/src/androidTest/java/com/Client/Table/ui/bio/EditBioFragmentTest.kt
+++ Client/app/src/androidTest/java/com/Client/Table/ui/bio/EditBioFragmentTest.kt
@@ -2,8 +2,7 @@ package com.Client.Table.ui.bio
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.Client.Table.R
@@ -15,12 +14,12 @@ class EditBioFragmentTest {
     fun testInputFields()
     {
         val scenario = launchFragmentInContainer<EditBioFragment>()
-        onView(withId(R.id.bio_username_input)).perform(typeText("tarik"))
-        onView(withId(R.id.bio_age_input)).perform(typeText("16"))
-        onView(withId(R.id.bio_city_input)).perform(typeText("Vienna"))
+        onView(withId(R.id.bio_username_input)).perform(typeText("tarik"), closeSoftKeyboard())
+        onView(withId(R.id.bio_age_input)).perform(typeText("16"), closeSoftKeyboard())
+        onView(withId(R.id.bio_city_input)).perform(typeText("Vienna"), closeSoftKeyboard())
 
-        onView(withId(R.id.edit_bio_checkbox_1)).perform(click()).check(matches(isChecked()))
-        onView(withId(R.id.edit_bio_checkbox_2)).perform(click()).check(matches(isChecked()))
+        onView(withId(R.id.edit_bio_checkbox_1)).perform(click(), closeSoftKeyboard()).check(matches(isChecked()))
+        onView(withId(R.id.edit_bio_checkbox_2)).perform(click(), closeSoftKeyboard()).check(matches(isChecked()))
     }
 
     @Test
