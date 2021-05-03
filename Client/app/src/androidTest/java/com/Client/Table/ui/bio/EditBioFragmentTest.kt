a9d984fa0b5874e7aa8f29c22e213c4d812cedc2
Christof Feischl <mail@christof-feischl.at>
2021-05-03 09:03:35 +0200

    L4G-8 Update test package and class name [RDZ, FC]

---
+++ Client/app/src/androidTest/java/com/Client/Table/ui/bio/EditBioFragmentTest.kt
@@ -0,0 +1,32 @@
+package com.Client.Table.ui.bio
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.Client.Table.R
+import org.junit.Test
+
+class EditBioFragmentTest {
+
+    @Test
+    fun testInputFields()
+    {
+        val scenario = launchFragmentInContainer<EditBioFragment>()
+        onView(withId(R.id.bio_username_input)).perform(typeText("tarik"))
+        onView(withId(R.id.bio_age_input)).perform(typeText("16"))
+        onView(withId(R.id.bio_city_input)).perform(typeText("Vienna"))
+
+        onView(withId(R.id.edit_bio_checkbox_1)).perform(click()).check(matches(isChecked()))
+        onView(withId(R.id.edit_bio_checkbox_2)).perform(click()).check(matches(isChecked()))
+    }
+
+    @Test
+    fun testPicture()
+    {
+
+    }
+
+}
--- Client/app/src/main/java/com/Client/Table/ui/bio/EditBioFragment.kt
+++ Client/app/src/main/java/com/Client/Table/ui/bio/EditBioFragment.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.Client.Table.R
-import com.Client.Table.ui.slideshow.SlideshowViewModel
 
 class EditBioFragment : Fragment() {
 
