abca226379b6afe5df52d579cf4dced1fe83b7ab
Dzem <rozajacdz@gmail.com>
2021-05-04 11:43:37 +0200

    L4G-8 tests for button implementation [RDZ,FC]

--- Client/app/src/androidTest/java/com/Client/Table/ui/bio/DisplayBioFragmentTest.kt
+++ Client/app/src/androidTest/java/com/Client/Table/ui/bio/DisplayBioFragmentTest.kt
@@ -2,6 +2,9 @@ package com.Client.Table.ui.bio
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.Client.Table.R
@@ -22,5 +25,28 @@ class DisplayBioFragmentTest {
         onView(withId(R.id.display_bio_preference_2)).check(matches(not(isDisplayed())))
         onView(withId(R.id.display_bio_preference_3)).check(matches(not(isDisplayed())))
         onView(withId(R.id.display_bio_preference_4)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun testEditProfileButton()
+    {
+        launchFragmentInContainer<DisplayBioFragment>()
+        onView(withId(R.id.display_bio_header)).check(matches(isDisplayed()))
+        onView(withId(R.id.edit_profile_button)).perform(click())
+        onView(withId(R.id.edit_bio_header)).check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun testProfileSubmitButton()
+    {
+       launchFragmentInContainer<EditBioFragment>()
+        onView(withId(R.id.bio_username_input)).perform(typeText("dzemail"))
+        onView(withId(R.id.bio_age_input)).perform(typeText("20"))
+        onView(withId(R.id.bio_age_input)).perform(typeText("Graz"))
+        onView(withId(R.id.edit_bio_header)).check(matches(isDisplayed()))
+        onView(withId(R.id.edit_bio_submit)).perform(click())
+        onView(withId(R.id.display_bio_header)).check(matches(isDisplayed()))
+
     }
 }
