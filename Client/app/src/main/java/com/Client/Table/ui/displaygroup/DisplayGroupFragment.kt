a980a7a2434eb9c0b7a5e1b515dbafa8ceaa937f
Jörg Werner Rainer <joerg.rainer@student.tugraz.at>
2021-05-17 16:37:25 +0200

    L4G-10 Added viewmodel implementation, errrorhandling, translation [BT,RJ]

--- Client/app/src/main/java/com/Client/Table/ui/displaygroup/DisplayGroupFragment.kt
+++ Client/app/src/main/java/com/Client/Table/ui/displaygroup/DisplayGroupFragment.kt
@@ -10,26 +10,27 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 import com.Client.Table.R
-import java.lang.reflect.Member
+import com.Client.Table.data.GroupDataSource
+import android.widget.*
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+
+
 
 class DisplayGroupFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = DisplayGroupFragment()
-    }
+    private lateinit var groupViewModel: DisplayGroupViewModel
 
-    private lateinit var viewModel: DisplayGroupViewModel
-
-    // --
-    lateinit var members : ArrayList<String>
-    var nameList = arrayListOf<String>()
-    // --
-
-    lateinit var groupName: EditText
-    lateinit var memberName: EditText
+    lateinit var members : MutableList<String>
+    lateinit var groupName: TextView
+    lateinit var memberName: TextView
     lateinit var saveGroupBtn: Button
     lateinit var addMemberBtn: Button
     lateinit var membersList: ListView
+    lateinit var membersAdapter: ArrayAdapter<String>;
+    var errorval: Int = -1;
+
+
 
 
     override fun onCreateView(
@@ -37,22 +38,47 @@ class DisplayGroupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragement_display_group, container, false)
-
+        groupViewModel = DisplayGroupViewModel()
         groupName = root.findViewById(R.id.groupNameText)
         memberName = root.findViewById(R.id.addPersonText)
         saveGroupBtn = root.findViewById(R.id.saveGroupBtn)
         addMemberBtn = root.findViewById(R.id.addPlayerBtn)
         membersList = root.findViewById(R.id.groupListView)
-
+        addMemberBtn.setOnClickListener {
+                groupViewModel.addMember(memberName.text.toString())
+                membersAdapter.notifyDataSetChanged()
+        }
+        membersList.setOnItemLongClickListener { parent, view, position, id ->
+            groupViewModel.deleteMember(id.toInt())
+            membersAdapter.notifyDataSetChanged()
+            true
+        }
+        members = ArrayList()
+        membersAdapter = ArrayAdapter(this.requireContext(), android.R.layout.simple_list_item_1, members)
+        membersList.adapter=membersAdapter
         return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DisplayGroupViewModel::class.java)
-        // TODO: Use the ViewModel
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+           // groupViewModel = ViewModelProvider(this).get(DisplayGroupViewModel::class.java)
+            // TODO: Use the ViewModel
+            groupViewModel.group_name.observe(viewLifecycleOwner, Observer { it ->
+                groupName.text = it
+            })
 
+             groupViewModel.error_.observe(viewLifecycleOwner, Observer { it ->
+                 errorval = it
+                 if(errorval!=-1){Toast.makeText(this.requireContext(), getString(errorval), Toast.LENGTH_SHORT).show()}
+             })
+
+            groupViewModel.group_members.observe(viewLifecycleOwner, Observer { it ->
+                members = it
+                membersAdapter = ArrayAdapter(this.requireContext(), android.R.layout.simple_list_item_1, members)
+                membersList.adapter=membersAdapter
+                membersAdapter.notifyDataSetChanged()
+            })
 
     }
 
--- Client/app/src/main/java/com/Client/Table/ui/displaygroup/DisplayGroupViewModel.kt
+++ Client/app/src/main/java/com/Client/Table/ui/displaygroup/DisplayGroupViewModel.kt
@@ -1,19 +1,28 @@
 package com.Client.Table.ui.displaygroup
-
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.Client.Table.R
+import com.Client.Table.data.GroupDataSource
+import com.Client.Table.data.Result
+import com.Client.Table.data.model.Group
+import com.Client.Table.ui.login.LoggedInUserView
+import com.Client.Table.ui.login.LoginResult
 
-class DisplayGroupViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class DisplayGroupViewModel() : ViewModel() {
+    private val dataSource: GroupDataSource = GroupDataSource()
     private val _group_name: MutableLiveData<String> = MutableLiveData<String>("Your Group Name")
     //private val _member_to_add: MutableLiveData<String> = MutableLiveData<String>()
     private val _member_list = MutableLiveData<MutableList<String>>(ArrayList())
+    private val _error_val: MutableLiveData<Int> = MutableLiveData<Int>(-1)
 
 
     val group_name: LiveData<String> get() = _group_name
     //val member_to_add_name: LiveData<String> get() = _member_to_add
     val group_members: LiveData<MutableList<String>> get() = _member_list
+    val error_ :LiveData<Int> get() = _error_val
 
     fun setData(name: String, member: String, members: MutableList<String>)
     {
@@ -22,10 +31,40 @@ class DisplayGroupViewModel : ViewModel() {
         _member_list.value = members
 
     }
+    fun fetchGroupData(name: String){
+        val data = dataSource.getGroup(name)
+
+        if (data is Result.Success) {
+            translateGroupDataSource(data.data)
+
+        } else {
+            _error_val.value = R.string.group_not_found
+
+        }
+
+    }
+
+    fun translateGroupDataSource(group: Group){
+        _group_name.value = group.GroupName
+        // _member_to_add.value = member
+        _member_list.value = ArrayList(group.fetchedUserList.map { it.name })
+    }
+
     fun addMember(name:String){
         _member_list.value?.let {
             val mylist: MutableList<String> = ArrayList<String>(_member_list.value)
-                mylist.add(name)
+            if (!mylist.contains(name))
+            {
+                if(mylist.size>=10){
+                    _error_val.value = R.string.too_many_members
+                }
+                else {
+                    mylist.add(name)
+                }
+            }
+            else{
+                _error_val.value = R.string.cannot_add_member_twice
+            }
             _member_list.value = mylist
         }
     }
@@ -33,7 +72,7 @@ class DisplayGroupViewModel : ViewModel() {
         _member_list.value?.let {
             val mylist: MutableList<String> = ArrayList<String>(_member_list.value)
             mylist.removeAt(id)
-
+            _error_val.value = R.string.deleted_member
             _member_list.value = mylist
         }
     }
