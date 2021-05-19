996cd198e0331d6750db17551cfdb198e86851d6
Dzemail Rozajac <rozajac@student.tugraz.at>
2021-05-19 09:20:02 +0200

    L4G-5 Added Age Range Slider test [HB, RDZ]

--- Client/app/src/test/java/com/Client/Table/SearchProfileTest.kt
+++ Client/app/src/test/java/com/Client/Table/SearchProfileTest.kt
@@ -1,12 +1,18 @@
 package com.Client.Table
 
 import android.view.KeyEvent
+import android.view.View
+import androidx.core.content.MimeTypeFilter.matches
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.ui.UiController
+import com.google.android.material.slider.Slider
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.Description
+import java.util.regex.Matcher
 
 
 class SearchProfileTest {
@@ -30,4 +36,32 @@ class SearchProfileTest {
         assertFalse(onView(withId(R.id.profile_search_view)).perform(pressKey(KeyEvent.KEYCODE_ENTER)))*/
 
     }
+
+
+    // Adapted from
+    // https://stackoverflow.com/questions/65390086/androidx-how-to-test-slider-in-ui-tests-espresso
+    // Adaptation start:
+    @Test
+    fun testSlider() {
+        onView(withId(R.id.sliderRange)).check(assert(withValue(25)))
+
+        onView(withId(R.id.sliderRange)).perform(setValue(30))
+
+        onView(withId(R.id.sliderRange)).check(assert(withValue(20)))
+    }
+
+    fun withValue(expectedValue: Int): ViewMatchers {
+        return object : BoundedMatcher<View?, Slider>(Slider::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("expected: $expectedValue")
+            }
+
+            override fun matchesSafely(slider: Slider?): Boolean {
+                return slider?.value?.toInt() == expectedValue
+            }
+        }
+    }
+}
+    // Adaptation End
+
 }
