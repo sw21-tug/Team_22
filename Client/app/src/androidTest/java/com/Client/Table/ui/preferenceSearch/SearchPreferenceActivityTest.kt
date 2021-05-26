f5854e65e1b08c089f295327f8befd7bb78351ae
Christof Feischl <mail@christof-feischl.at>
2021-05-26 11:56:33 +0200

    L4G-14 Add testcase for search preference result list [HB, FC]

--- Client/app/src/androidTest/java/com/Client/Table/ui/preferenceSearch/SearchPreferenceActivityTest.kt
+++ Client/app/src/androidTest/java/com/Client/Table/ui/preferenceSearch/SearchPreferenceActivityTest.kt
@@ -1,17 +1,23 @@
 package com.Client.Table.ui.preferenceSearch
 
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.PerformException
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import com.Client.Table.R
 import com.Client.Table.data.LoginRepository
 import com.Client.Table.ui.login.LoggedInUserView
 import com.google.android.material.slider.RangeSlider
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.junit.Before
 import org.junit.Test
@@ -35,6 +41,20 @@ class SearchPreferenceActivityTest {
 
             override fun getConstraints(): Matcher<View> {
                 return isAssignableFrom(RangeSlider::class.java)
+            }
+        }
+    }
+
+    private fun isRecyclerViewEmpty(): Matcher<View?>? {
+        return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("is empty?")
+            }
+
+            override fun matchesSafely(recyclerView: RecyclerView): Boolean {
+                if (recyclerView.adapter == null)
+                    return false
+                return recyclerView.adapter!!.itemCount > 0
             }
         }
     }
@@ -81,6 +101,36 @@ class SearchPreferenceActivityTest {
         onView((withId(R.id.search_profile_submit_btn))).perform(click())
 
         onView(withId(R.id.search_profile_error_message)).check(matches(isDisplayed()))
+    }
 
+    @Test(expected = PerformException::class)
+    fun listNotShownUntilClicked() {
+        ActivityScenario.launch(SearchPreferenceActivity::class.java)
+        onView(ViewMatchers.withId(R.id.search_preference_list))
+            .check(matches(isRecyclerViewEmpty()))
+    }
+
+    @Test
+    fun listShownAfterSubmitClicked() {
+        ActivityScenario.launch(SearchPreferenceActivity::class.java)
+
+        onView(withId(R.id.search_profile_board_games)).perform(click(), closeSoftKeyboard()).check(
+            matches(
+                isChecked()
+            )
+        )
+
+        onView((withId(R.id.search_profile_age_slider))).perform(setValueRangeSlider(), closeSoftKeyboard())
+
+        onView(withId(R.id.search_profile_city_input)).perform(typeText("Graz"), closeSoftKeyboard()).check(
+            matches(
+                withText("Graz")
+            )
+        )
+
+        onView((withId(R.id.search_profile_submit_btn))).perform(click())
+
+        onView(ViewMatchers.withId(R.id.search_preference_list))
+            .check(matches(not(isRecyclerViewEmpty())))
     }
 }
