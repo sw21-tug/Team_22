49a5dc5ae72ecfa1abc09eebe44c8dc9024b04cc
Tarik Barucija <tarikbarucija@outlook.com>
2021-05-26 10:40:56 +0200

    L4G-7 first test case [BT, TL]

---
+++ DataProxy/test/SearchPreferencesTest.kt
@@ -0,0 +1,75 @@
+package com.Table.Server
+
+import com.Table.Server.DatabaseObjects.Bio
+import com.Table.Server.DatabaseObjects.User
+import com.Table.Server.DatabaseObjects.UserCredentials
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.convertValue
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
+import io.ktor.server.testing.withTestApplication
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class SearchPreferencesTest {
+
+    val dbConnector: DatabaseConnector = DatabaseConnector()
+
+    var loggedInUser: User = User(null, "maxi", "mustermann", "password")
+    var jwtToken: String? = null
+    val jwthandler = JWTHandler()
+
+    @Before
+    fun initDB() {
+        dbConnector.reset()
+        dbConnector.connectToDatabase()
+        val id = dbConnector.insertUser(loggedInUser)
+        jwtToken = "Bearer " + jwthandler.generateLoginToken(dbConnector.getUserForAuth(UserCredentials(loggedInUser.username, loggedInUser.password, null))[0])
+        loggedInUser = dbConnector.getUserById(id)[0]
+    }
+
+    @Test
+    fun testSimpleSearch() {
+        val mapper = jacksonObjectMapper()
+        withTestApplication({ module(testing = true) }) {
+            val update_bio_request = handleRequest(HttpMethod.Post, "/user/updateBio") {
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                addHeader(HttpHeaders.Authorization, jwtToken!!)
+                setBody(jacksonObjectMapper().writeValueAsString(Bio(loggedInUser.bio_id, "maxi_muster", 16, "Graz", true, false, true, false)))
+            }
+
+            update_bio_request.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+
+            val search_request = handleRequest(HttpMethod.Post, "/user/getUsersByPreferences") {
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                addHeader(HttpHeaders.Authorization, jwtToken!!)
+                setBody(jacksonObjectMapper().writeValueAsString(SearchPreferences(16, "Graz", true, false, true, false)))
+            }
+
+            search_request.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                try {
+                    val tree: JsonNode = mapper.readTree(response.content)
+                    val mapperConvertValue:User = mapper.convertValue<User>(tree)
+                    assertTrue(mapperConvertValue.username == "maxi_muster")
+                }
+                catch (e:Exception)
+                {
+                    println("Testcase Failed")
+                    assert(false)
+                }
+            }
+
+        }
+    }
+    
+}
